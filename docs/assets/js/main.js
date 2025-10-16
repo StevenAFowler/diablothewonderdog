aa0f97ee1ffc7ee42668d510eaf7ca41d5bc52a3
StevenAFowler <steven.a.fowler@gmail.com>
2025-10-15 23:04:48 -0700

    Update with animation and landing page

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -10,6 +10,22 @@ function close_article(artID)
     document.querySelector('.blur_overlay').style.display='none';
 }
 
+// Animation path lengths
+const path_objects = {dog: '.svg_dog', 
+                      cir: '.svg_cir', 
+                      dow: '.svg_dow',
+                      l11: '.svg_l11',
+                      l12: '.svg_l12',
+                      l21: '.svg_l21',
+                      l22: '.svg_l22'}
+Object.values(path_objects).forEach(obj => {
+    var path = document.querySelector(obj);
+    var length = path.getTotalLength();
+    path.style.strokeDasharray = length + ' ' + length;
+    path.style.strokeDashoffset = length;
+    console.log(obj, ' - ', length);
+});
+
 // Reload at top of page
 window.addEventListener('load', function() {
     setTimeout(() => {
